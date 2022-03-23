a159c6546487145231c971426526b2e01dfb1107
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-03-23 23:23:48 +0530

    added theory about the linear search and time complexity

--- Linear_Sarch_algorithm/src/com/adnan/Main.java
+++ Linear_Sarch_algorithm/src/com/adnan/Main.java
@@ -3,6 +3,32 @@ package com.adnan;
 public class Main {
 
     public static void main(String[] args) {
+        // linear searching means searching something in a given direction
+        // and searching in a collection datastructure like array, or string
+
+
+
+        //=======================================================================
+
+//        arr = {1,2,3,4,5,6,7,8,9}
+//        Q: Find whether 4 exists in the arr array??
+//        Ans: firstly iterate over the given array and search for the value on each index
+//             after that if the value is found then return the index of that value or return -1;;
+//         =================================================================================
+
+//        ABOUT : Time Complexity
+/*       1. time complexity is calculated by the searching time taken by our algorithm to search any item
+//          given in a dataset..
+//       2. in the best case we will get our item at the first index which will be completed in minimum time
+//          in  this type of case the length of the array does not matters as we are getting our
+//          result/target value at the first index/place
+//          so the  big 'O' notation for this case will be O(1)
+//       3. in the worst case where we iterated through the whole array of thousands items or millions of items
+//          but still failed to find the target , here the time is directly proportional to the length of the array
+//          for this we get a linear graph where time increases with the increase in size of the array..
+//
+
+ */
         System.out.println();
     }
 }
