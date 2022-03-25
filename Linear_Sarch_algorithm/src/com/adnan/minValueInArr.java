cde4d907ce3cf505fcd4ad20286b5550f9c49d0b
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-03-25 07:44:40 +0530

    made the minvalueofarr function

---
+++ Linear_Sarch_algorithm/src/com/adnan/minValueInArr.java
@@ -0,0 +1,19 @@
+package com.adnan;
+
+public class minValueInArr {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println(min(arr));
+    }
+
+    static int min(int[] arr) {
+        int ans = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if(ans > arr[i]){
+                ans = arr[i];
+            }
+
+        }
+        return ans;
+    }
+}
