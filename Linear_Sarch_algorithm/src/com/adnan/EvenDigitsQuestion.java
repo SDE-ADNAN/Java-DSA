781312729d16172d030a321022a09f00332c3040
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-03-26 00:27:06 +0530

    solved the first leetcode problem on even digits array item

---
+++ Linear_Sarch_algorithm/src/com/adnan/EvenDigitsQuestion.java
@@ -0,0 +1,84 @@
+package com.adnan;
+
+// https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+
+/*
+* 1295. Find Numbers with Even Number of Digits
+Easy
+
+1186
+
+97
+
+Add to List
+
+Share
+Given an array nums of integers, return how many of them contain an even number of digits.
+
+
+
+Example 1:
+
+Input: nums = [12,345,2,6,7896]
+Output: 2
+Explanation:
+12 contains 2 digits (even number of digits).
+345 contains 3 digits (odd number of digits).
+2 contains 1 digit (odd number of digits).
+6 contains 1 digit (odd number of digits).
+7896 contains 4 digits (even number of digits).
+Therefore only 12 and 7896 contain an even number of digits.
+Example 2:
+
+Input: nums = [555,901,482,1771]
+Output: 1
+Explanation:
+Only 1771 contains an even number of digits.
+
+
+Constraints:
+
+1 <= nums.length <= 500
+1 <= nums[i] <= 105  */
+
+public class EvenDigitsQuestion {
+    public static void main(String[] args) {
+        int[] nums = {12,345,2,6,7896};
+
+        System.out.println(findNumber(nums));
+    }
+    static int findNumber(int[] nums){
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(even(nums[i])){
+                count++;
+            }
+
+        }
+        return count;
+
+    }
+
+//    check if num is even
+    static boolean even(int num){
+        int noofDigit = digits(num);
+        return noofDigit % 2 == 0;
+    }
+
+    static int digits(int num){
+        int count = 0;
+        if(num<0){
+            num = num * -1;
+        }
+        if(num == 0){
+            return 1;
+        }
+
+
+        while(num>0){
+            count++;
+            num = num/10;
+        }
+        return count;
+    }
+}
