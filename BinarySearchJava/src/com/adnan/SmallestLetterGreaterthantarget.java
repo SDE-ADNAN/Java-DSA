a8d3be29dc3c6c1f44b563f2deda936a422aeece
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-03-28 00:35:37 +0530

    made some more modifications in the 'smallest latter Gratest than target'

--- BinarySearchJava/src/com/adnan/SmallestLetterGreaterthantarget.java
+++ BinarySearchJava/src/com/adnan/SmallestLetterGreaterthantarget.java
@@ -5,34 +5,26 @@ package com.adnan;
 public class SmallestLetterGreaterthantarget {
     public static void main(String[] args) {
         char[] letters ={'c','f','j'};
-        char target = 'f';
+        char target = 'k';
         char ans2 = smallestGreatest(letters,target); // OABS==OrderAgnosticBInarySearch
-
         System.out.println(ans2);
     }
     // return the index of smallest no >= target
     private static char smallestGreatest(char[] letters, char target) {
-
-
-
         int start = 0;
         int end = letters.length -1;
-        boolean IsAsccending = letters[start]<letters[end];
-
         while(start <=end){
-//         find the middle element
-//            int mid = (start+end)/2; // this may lead to mid exceeding the max capacity of int and throw error;
             int mid = start+(end-start)/2;
-
-            if(IsAsccending){
                 if(target < letters[mid]){
                     end = mid-1;
                 }else{
                     start = mid +1;
                 }
-
-            }
         }
+//        here start  is from the time when the while loops gets violated means letters array .length
+//         that is start = letters.length(equal to 3)
+//        thats why start % letters.length == 0
+//               as     3 % 3 ==0
         return letters[start % letters.length];
     }
 }
