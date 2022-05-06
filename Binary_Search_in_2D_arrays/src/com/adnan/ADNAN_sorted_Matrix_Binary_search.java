8f208e711100ab52ca06eb0f7b48e2e9fad240b9
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-05-06 23:26:08 +0530

    started with bubble sort

--- Binary_Search_in_2D_arrays/src/com/adnan/ADNAN_sorted_Matrix_Binary_search.java
+++ Binary_Search_in_2D_arrays/src/com/adnan/ADNAN_sorted_Matrix_Binary_search.java
@@ -4,14 +4,17 @@ import java.util.Arrays;
 
 public class ADNAN_sorted_Matrix_Binary_search {
     public static void main(String[] args) {
-        int[][] arr={
-            {1,2,3,4},
-            {5,6,7,8},
-            {9,10,11,12},
-            {13,14,15,16},
-            {17,18,19,20},
-    };
+//        int[][] arr={
+//            {1,2,3,4},
+//            {5,6,7,8},
+//            {9,10,11,12},
+//            {13,14,15,16},
+//            {17,18,19,20},
+//        };
+        int[][] arr={{1}};
         System.out.println(Arrays.toString(innerBs(arr,10)));
+        boolean isPresent = false;
+
     }
     static int outerBS (int[][]arr,int target){
         int start = 0;
--- Binary_Search_in_2D_arrays/src/com/adnan/SORTED_Matrix_Binary_Search.java
+++ Binary_Search_in_2D_arrays/src/com/adnan/SORTED_Matrix_Binary_Search.java
@@ -4,11 +4,12 @@ import java.util.Arrays;
 
 public class SORTED_Matrix_Binary_Search {
     public static void main(String[] args) {
-        int[][] arr = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}
-        };
+//        int[][] arr = {
+//                {1, 2, 3},
+//                {4, 5, 6},
+//                {7, 8, 9}
+//        };
+        int[][] arr={{1}};
         System.out.println(Arrays.toString(search(arr, 9)));
     }
 
