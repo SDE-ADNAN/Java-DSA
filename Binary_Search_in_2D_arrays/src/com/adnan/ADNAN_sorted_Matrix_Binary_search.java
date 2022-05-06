ee1293430409417cd63b3b52b50d810e4ffa60f4
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-05-06 18:53:19 +0530

    trying to dev a simple algo for searching in the sorted matrix with help of binarySearch

---
+++ Binary_Search_in_2D_arrays/src/com/adnan/ADNAN_sorted_Matrix_Binary_search.java
@@ -0,0 +1,35 @@
+package com.adnan;
+
+import java.util.Arrays;
+
+public class ADNAN_sorted_Matrix_Binary_search {
+    public static void main(String[] args) {
+        int[][] arr={
+            {1,2,3,4},
+            {5,6,7,8},
+            {9,10,11,12},
+            {13,14,15,16},
+            {17,18,19,20},
+    };
+        System.out.println(Arrays.toString(outerBS(arr,10)));
+    }
+    static int[] outerBS (int[][]arr,int target){
+        int start = 0;
+        int end = arr.length -1;
+        int rStart = arr[start][0];
+        int rEnd = arr[0].length -1;
+        while(start<=end){
+            int mid = start + (end -start)/2;
+            if(arr[mid][rStart] <=target && arr[mid][rEnd]>=target){
+                return arr[mid];
+            }
+            if(arr[mid][rStart] <=target && arr[mid][rEnd] <=target){
+                start = mid +1;
+            }else if(arr[start][rStart] >=target && arr[start][rEnd]>=target){
+                end = mid-1;
+            }
+
+        }
+        return new int[]{-1,-1};
+    }
+}
