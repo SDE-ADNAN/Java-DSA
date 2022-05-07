14ce9705fadcea4e568a53c1ab6da47b0e025ac4
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-05-07 23:34:32 +0530

    solved the index out of bounds exception but getting the 'time limit exceeded ' exception on leetcode

--- Binary_Search_in_2D_arrays/src/com/adnan/ADNAN_sorted_Matrix_Binary_search.java
+++ Binary_Search_in_2D_arrays/src/com/adnan/ADNAN_sorted_Matrix_Binary_search.java
@@ -5,11 +5,11 @@ import java.util.Arrays;
 public class ADNAN_sorted_Matrix_Binary_search {
     public static void main(String[] args) {
 //        int[][] arr={
-//            {1,2,3,4},
+//            {0,2,3,4},
 //            {5,6,7,8},
 //            {9,10,11,12},
 //            {13,14,15,16},
-//            {17,18,19,20},
+//            {18,19,20,99},
 //        };
         int[][] arr={{1}};
         System.out.println(Arrays.toString(innerBs(arr,10)));
@@ -19,16 +19,17 @@ public class ADNAN_sorted_Matrix_Binary_search {
     static int outerBS (int[][]arr,int target){
         int start = 0;
         int end = arr.length -1;
+        // here rStart
         int rStart = arr[start][0];
         int rEnd = arr[0].length -1;
         while(start<=end){
             int mid = start + (end -start)/2;
-            if(arr[mid][rStart] <=target && arr[mid][rEnd]>=target){
+            if(arr[mid][0] <=target && arr[mid][arr[mid].length-1]>=target){
                 return mid;
             }
-            if(arr[mid][rStart] <=target && arr[mid][rEnd] <=target){
+            if(arr[mid][0] <=target && arr[mid][arr[mid].length-1] <=target){
                 start = mid +1;
-            }else if(arr[start][rStart] >=target && arr[start][rEnd]>=target){
+            }else if(arr[start][0] >=target && arr[start][arr[mid].length-1]>=target){
                 end = mid-1;
             }
 
@@ -37,6 +38,9 @@ public class ADNAN_sorted_Matrix_Binary_search {
     }
     static int[] innerBs(int[][]arr,int target){
         int ansArr = outerBS(arr,target);
+        if(ansArr == -1){
+            return  new int[] {-1,-1};
+        }
         int start = 0;
         int end = arr[0].length -1;;
         while(start<=end){
