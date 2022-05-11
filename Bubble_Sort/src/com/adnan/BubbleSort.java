174b2ebda590aa458221662267bea3e8f8f2e858
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-05-11 20:20:42 +0530

    added more exp to bubble sorting algo

--- Bubble_Sort/src/com/adnan/BubbleSort.java
+++ Bubble_Sort/src/com/adnan/BubbleSort.java
@@ -23,7 +23,7 @@ public class BubbleSort {
             for (int j = 1; j < arr.length - i; j++) {
                 // swap if the item is smaller than the previous item
                 if (arr[j] < arr[j-1]) {
-                    // swapping logic
+                    // swapping logic most imp for sorting algorithms
                     int temp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] = temp;
