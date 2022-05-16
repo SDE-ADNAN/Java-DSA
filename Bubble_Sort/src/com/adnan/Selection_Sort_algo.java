1006b361fbfea237c286732b561a8ef21c8265a4
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-05-16 23:44:34 +0530

    started with selection sort after completing  bubble sort

---
+++ Bubble_Sort/src/com/adnan/Selection_Sort_algo.java
@@ -0,0 +1,13 @@
+package com.adnan;
+
+import java.util.Arrays;
+
+public class Selection_Sort_algo {
+    public static void main(String[] args) {
+        int[] arr = {3,54,67,5,68,2,44,12,};
+        System.out.println(Arrays.toString(arr));
+    }
+
+    
+
+}
