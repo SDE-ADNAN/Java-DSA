8e6d0b749c5915799dae30715d0f2b69ea38463d
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-05-17 00:07:30 +0530

    added the bubblesort and selection sort

--- Bubble_Sort/src/com/adnan/Selection_Sort_algo.java
+++ Bubble_Sort/src/com/adnan/Selection_Sort_algo.java
@@ -8,6 +8,22 @@ public class Selection_Sort_algo {
         System.out.println(Arrays.toString(arr));
     }
 
-    
+    static void selectionSort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            // find the max element from the array and place it inthe end of the array ..
+            int last = arr.length-i-1;
+            int max = getMaxIndex(arr,0,last);
+        }
+    }
+
+    private static int getMaxIndex(int[] arr,int start, int end) {
+        int max = start;
+        for (int i = start; i <= end ; i++) {
+            if(arr[max]< arr[i]){
+                max = i;
+            }
+            
+        }
+    }
 
 }
