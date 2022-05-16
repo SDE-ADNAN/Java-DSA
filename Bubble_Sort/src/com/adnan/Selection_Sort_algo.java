483d5130b128395397e98d36dbf8c2d1e8af22bb
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-05-17 00:19:16 +0530

    added the full code of selection sort algo

--- Bubble_Sort/src/com/adnan/Selection_Sort_algo.java
+++ Bubble_Sort/src/com/adnan/Selection_Sort_algo.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class Selection_Sort_algo {
     public static void main(String[] args) {
         int[] arr = {3,54,67,5,68,2,44,12,};
+        selectionSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -13,17 +14,25 @@ public class Selection_Sort_algo {
             // find the max element from the array and place it inthe end of the array ..
             int last = arr.length-i-1;
             int max = getMaxIndex(arr,0,last);
+            swap(arr,max,last);
         }
     }
 
-    private static int getMaxIndex(int[] arr,int start, int end) {
+    static int getMaxIndex(int[] arr,int start, int end) {
         int max = start;
         for (int i = start; i <= end ; i++) {
             if(arr[max]< arr[i]){
                 max = i;
             }
-            
         }
+        return max ;
     }
+    static void swap(int[] arr,int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+
 
 }
