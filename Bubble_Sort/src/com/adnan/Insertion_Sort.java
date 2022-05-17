fbb52c7e8c0bb58debe23a85337217174b90f49f
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-05-17 19:09:13 +0530

    done with insertion sort

--- Bubble_Sort/src/com/adnan/Insertion_Sort.java
+++ Bubble_Sort/src/com/adnan/Insertion_Sort.java
@@ -1,8 +1,29 @@
 package com.adnan;
 
+import java.util.Arrays;
+
 public class Insertion_Sort {
     public static void main(String[] args) {
-
+        int[] arr = {3,54,67,5,68,2,44,12,};
+        insertion(arr);
+        System.out.println(Arrays.toString(arr));
     }
-    static
+
+    static void insertion(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i+1; j >0 ; --j) {
+                if(arr[j]<arr[j-1]){
+                    swap(arr,j,j-1);
+                }else{
+                    break;
+                }
+            }
+        }
+    }
+    // separate function for swapping the elements..
+    static void swap(int[] arr,int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
 }
