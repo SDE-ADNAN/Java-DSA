f435068f5462555093a18aa94dafc2360be55755
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-05-17 20:21:01 +0530

    done with cyclic sort

--- Bubble_Sort/src/com/adnan/Cyclic_Sort.java
+++ Bubble_Sort/src/com/adnan/Cyclic_Sort.java
@@ -4,11 +4,24 @@ import java.util.Arrays;
 
 public class Cyclic_Sort {
     public static void main(String[] args) {
-        int[] arr = {3,54,67,5,68,2,44,12,};
+        // cyclic sort is only for range of numbers from 1 to n with all unique items
+        // means u should know the start and end of the range..
+        int[] arr = {3,5,2,1,4};
         Cyclic(arr);
         System.out.println(Arrays.toString(arr));
     }
 
+    static void Cyclic(int[] arr){
+        int i = 0;
+        while(i<arr.length){
+            int correct = arr[i]-1;
+            if(arr[i]!=arr[correct]) {
+                swap(arr, i, correct);
+            }else{
+                i++;
+            }
+        }
+    }
 
 
     // separate function for swapping the elements..
