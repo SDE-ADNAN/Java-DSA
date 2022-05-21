a39d7df2d994261d367185d4c383c0b46f6a7255
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-05-21 18:53:33 +0530

    added the explaination for the setmismatch problem

--- Bubble_Sort/src/com/adnan/SetMismatch_leetcode_645.java
+++ Bubble_Sort/src/com/adnan/SetMismatch_leetcode_645.java
@@ -21,10 +21,14 @@ public class SetMismatch_leetcode_645 {
             }
         }
 
+        // only changed this section
         for (int index = 0; index < arr.length; index++) {
             // searching for the misplaced item
             if (arr[index] != index+1) {
-                // adding it to the ans arraylist
+                // adding it to the new array we are returning.
+                // this array has the element which is duplicated at its 0th index
+                // and element which is not present at its place (but the duplicate element has
+                // acquired its place) is at 1st place.
                 return new int[] {arr[index],index+1};
             }
         }
