2c82a863c9e532c412dc3f3517028c47c42894ec
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-05-21 16:22:34 +0530

    done the code for find all duplicates from an array

--- Bubble_Sort/src/com/adnan/Find_all_duplicate_elements.java
+++ Bubble_Sort/src/com/adnan/Find_all_duplicate_elements.java
@@ -1,9 +1,40 @@
 package com.adnan;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Find_all_duplicate_elements {
     // leetcode #442 Find all duplicate elements.
     public static void main(String[] args) {
-
+     int[] arr ={4,3,2,7,8,2,3,1};
+        System.out.println(findDuplicates(arr));
     }
-    
+    static List<Integer> findDuplicates(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]) {
+                swap(arr, i , correct);
+            } else {
+                i++;
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index+1) {
+                ans.add(arr[index]);
+            }
+        }
+
+        return ans;
+    }
+
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
 }
