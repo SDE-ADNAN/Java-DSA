36f8fa9e843b6c7f0d4bea14aaa3d950362804b7
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-05-21 23:30:20 +0530

    started with the leetcode 41 first missing positive problem

---
+++ Bubble_Sort/src/com/adnan/firstMissingPositive.java
@@ -0,0 +1,9 @@
+package com.adnan;
+
+public class firstMissingPositive {
+    //leet code 41 find missing positive.
+    public static void main(String[] args) {
+        int[] arr={3,4,-1,1};
+    }
+
+}
