fe03bfa2c59335f89b3c25c7305e346fab79bfa9
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-05-21 18:49:28 +0530

    done code for setmismatch problem

--- Bubble_Sort/src/com/adnan/SetMismatch_leetcode_645.java
+++ Bubble_Sort/src/com/adnan/SetMismatch_leetcode_645.java
@@ -7,10 +7,35 @@ import java.util.List;
 public class SetMismatch_leetcode_645 {
     public static void main(String[] args) {
         int[] arr ={1,1};
+        System.out.println(Arrays.toString(findDuplicates(arr)));
+    }
+
+    static int[] findDuplicates(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]) {
+                swap(arr, i , correct);
+            } else {
+                i++;
+            }
+        }
+
+        for (int index = 0; index < arr.length; index++) {
+            // searching for the misplaced item
+            if (arr[index] != index+1) {
+                // adding it to the ans arraylist
+                return new int[] {arr[index],index+1};
+            }
+        }
+
+        return new int[] {-1,-1};
     }
 
 
-
-
-
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
 }
