f31c89520c15ad278188960064f58527acf06803
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-05-23 23:48:53 +0530

    added notes about strings iteration and immutability

---
+++ Strings_AND_StringBuilders/src/com/adnan/strings_and_stringbuilders_bullet_notes.java
@@ -0,0 +1,24 @@
+package com.adnan;
+
+public class strings_and_stringbuilders_bullet_notes {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    /*
+
+    1. strings are just like arrays and we can iterate over them like arrays with help of indexing
+
+    2. strings are immutable in java for some of security reasons.
+       as if u declare a
+       var a ="adnan"
+       and then ur accessing it by a different var b
+       and if somehow this var a got changed then the value reffered by the var b will also get changed
+       (if both var a and b are pointing towards the same string in the heap memory )
+       these variables are stored in the stack memory but the string object is stored in the heap memory.
+
+     3.
+    */
+
+
+}
