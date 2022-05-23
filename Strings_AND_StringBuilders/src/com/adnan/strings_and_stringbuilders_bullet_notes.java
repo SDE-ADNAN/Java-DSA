f89b13310a69434d49637f341da638c484e9d2c9
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-05-24 00:12:52 +0530

    added about creating new string objs of same value and .equals method

--- Strings_AND_StringBuilders/src/com/adnan/strings_and_stringbuilders_bullet_notes.java
+++ Strings_AND_StringBuilders/src/com/adnan/strings_and_stringbuilders_bullet_notes.java
@@ -3,7 +3,9 @@ package com.adnan;
 public class strings_and_stringbuilders_bullet_notes {
 
     public static void main(String[] args) {
-	// write your code here
+        String A = new String {
+            "adnan"
+        };
     }
     /*
 
@@ -27,7 +29,36 @@ public class strings_and_stringbuilders_bullet_notes {
         once the string object is now no more accessed by any variable it is cleared through
         garbage collection.
 
-     4. 
+     4. How to create different string objects of same value ?
+  Ans:- we will use the new keyword for creating a new String of same type
+        eg.
+        String a = new String("adnan");
+        String b = new String("adnan");
+        this will not create the adnan string object in the string pool and access it for both a and b
+        but it will create it normally in the heap memory.
+
+     5. When u only need to check/compare the value of to Strings u must use the .equals() method of the
+        java String class.
+
+     6.  
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////
+Since, Java String is immutable and final, so a new String is created whenever we
+do String manipulation. As String manipulations are resource consuming,
+Java provides two utility classes: StringBuffer and StringBuilder.
+Let us understand the difference between these two utility classes:
+
+1. StringBuffer and StringBuilder are mutable classes. StringBuffer operations are
+   thread-safe and synchronized whereas StringBuilder operations are not thread-safe.
+2. StringBuffer is to be used when multiple threads are working on same String and
+   StringBuilder in the single threaded environment.
+3. StringBuilder performance is faster when compared to StringBuffer because of no
+   overhead of synchronized.
+///////////////////////////////////////////////////////////////////////////////////
     */
 
 
