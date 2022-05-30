e48841f7b2a9dd63b136408316ca86b13bdbd5a2
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-05-31 00:23:04 +0530

    doe untill format specifires

--- Strings_AND_StringBuilders/src/com/adnan/Pretty_printing_in_java.java
+++ Strings_AND_StringBuilders/src/com/adnan/Pretty_printing_in_java.java
@@ -14,5 +14,26 @@ public class Pretty_printing_in_java {
 
 
         System.out.printf("hey my name is %s and i am a %s","adnan","youtuber");
+
+
+
+        /*
+
+        Format specifiers:-
+Format Specifier	            Data Type	                                                           Output
+         %a	           floating point (except BigDecimal)	                            Returns Hex output of floating point number.
+         %b	            Any type	                                                    "true" if non-null, "false" if null
+         %c	            character	                                                    Unicode character
+         %d	            integer (incl. byte, short, int, long, bigint)	                Decimal Integer
+         %e	             floating point	                                                decimal number in scientific notation
+         %f	             floating point	                                                decimal number
+         %g	            floating point	                                                decimal number, possibly in scientific notation depending on the precision and value.
+         %h	             any type	                                                    Hex String of value from hashCode() method.
+         %n	            none	                                                        Platform-specific line separator.
+         %o	            integer (incl. byte, short, int, long, bigint)	                Octal number
+         %s	            any type	                                                    String value
+         %t	             Date/Time (incl. long, Calendar, Date and TemporalAccessor)	%t is the prefix for Date/Time conversions. More formatting flags are needed after this. See Date/Time conversion below.
+         %x	              integer (incl. byte, short, int, long, bigint)                Hex string.
+          */
     }
 }
