fd8ec4bf3c6cd25f475d760d4c5de58874e87bd2
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-06-01 17:05:59 +0530

    done strings and string builder till 49:50 with some operators

--- Strings_AND_StringBuilders/src/com/adnan/operators.java
+++ Strings_AND_StringBuilders/src/com/adnan/operators.java
@@ -2,6 +2,10 @@ package com.adnan;
 
 public class operators {
     public static void main(String[] args) {
-        
+        System.out.println('a'+0); //value of 'a' = 97(ascii value)
+        System.out.println('b'+0); // value of 'b' = 98(ascii value)
+        System.out.println('a'+'b'); // gives 195(ascii value) in output
+        System.out.println("a" + "b"); // gives "ab" in output which is a string
+        System.out.println('a'+3);// gives 100 in output ..
     }
 }
