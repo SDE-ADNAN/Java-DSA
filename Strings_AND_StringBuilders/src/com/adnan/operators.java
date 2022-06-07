276097c12e7836e67deab3bc0f89add5116d75cb
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-06-08 00:35:33 +0530

    done with the string concatination operator

--- Strings_AND_StringBuilders/src/com/adnan/operators.java
+++ Strings_AND_StringBuilders/src/com/adnan/operators.java
@@ -1,11 +1,27 @@
 package com.adnan;
 
+import java.util.ArrayList;
+
 public class operators {
     public static void main(String[] args) {
         System.out.println('a'+0); //value of 'a' = 97(ascii value)
         System.out.println('b'+0); // value of 'b' = 98(ascii value)
         System.out.println('a'+'b'); // gives 195(ascii value) in output
+        System.out.println((char)('a'+3)); // gives d(103) in output
         System.out.println("a" + "b"); // gives "ab" in output which is a string
         System.out.println('a'+3);// gives 100 in output .
+
+
+        System.out.println('a'+1);
+        // this is same as after few steps :'a' + 1
+        // integer will be converted to Integer which will further call the toString() method:
+
+        // if we con cat any class  object to a string it will be first converted to string
+        // by calling the toString() method on it .
+        System.out.println("adnan" + new ArrayList<>());// output : adnan[]
+        System.out.println("adnan" + new Integer(56));// output : adnan56
+
+
+        
     }
 }
