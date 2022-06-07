516fb680397a44a748e9bba5fe8f500d6f9ab6ff
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-06-07 23:53:00 +0530

    started with the course again

--- Strings_AND_StringBuilders/src/com/adnan/operators.java
+++ Strings_AND_StringBuilders/src/com/adnan/operators.java
@@ -6,6 +6,6 @@ public class operators {
         System.out.println('b'+0); // value of 'b' = 98(ascii value)
         System.out.println('a'+'b'); // gives 195(ascii value) in output
         System.out.println("a" + "b"); // gives "ab" in output which is a string
-        System.out.println('a'+3);// gives 100 in output ..
+        System.out.println('a'+3);// gives 100 in output .
     }
 }
