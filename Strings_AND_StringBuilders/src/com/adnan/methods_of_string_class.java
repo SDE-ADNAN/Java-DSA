f8b2c28e6e93b340178675340a2265fc16d366b6
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-06-09 22:23:41 +0530

    added the exp for the str methods code

--- Strings_AND_StringBuilders/src/com/adnan/methods_of_string_class.java
+++ Strings_AND_StringBuilders/src/com/adnan/methods_of_string_class.java
@@ -8,10 +8,14 @@ public class methods_of_string_class {
         String name = "Kunal Kushwaha Hello World";
         // here it is converting it to a character array
         System.out.println(Arrays.toString(name.toCharArray()));
+        // all the characters of the name str will be made uppercase chars
         System.out.println(name.toLowerCase());
         System.out.println(name);
+        // will give the index on which char a occured for first time
         System.out.println(name.indexOf('a'));
+        // will strip the white spaces from the string
         System.out.println("     Kunal   ".strip());
+        // will split the str into array elements disconnected from the place where a space was present
         System.out.println(Arrays.toString(name.split(" ")));
     }
 }
