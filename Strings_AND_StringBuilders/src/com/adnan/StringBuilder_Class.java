43eda93e869a09655edd65fbd4d73137f6b38b46
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-06-09 22:17:32 +0530

    added the exp for string builder class

--- Strings_AND_StringBuilders/src/com/adnan/StringBuilder_Class.java
+++ Strings_AND_StringBuilders/src/com/adnan/StringBuilder_Class.java
@@ -1,6 +1,16 @@
 package com.adnan;
 
 public class StringBuilder_Class {
+
+    /*
+    here we are using the string builder class to perform the append method
+    with help of a for loop which rather that creating a new object in the heap
+    like the string class , the string builder class performs all the operation on the same
+    stringbuilder object which is beneficial for saving memory space and time required.
+
+
+
+        */
     public static void main(String[] args) {
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < 26; i++) {
