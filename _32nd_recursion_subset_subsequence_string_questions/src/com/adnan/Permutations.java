d43ba51ff0af37870f67161ddac913366379db1c
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-09-26 00:38:11 +0530

    added the permutations file with the permutations method to return all possible permutations

---
+++ _32nd_recursion_subset_subsequence_string_questions/src/com/adnan/Permutations.java
@@ -0,0 +1,19 @@
+package com.adnan;
+
+public class Permutations {
+    public static void main(String[] args) {
+
+    }
+    static void permutations(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        for (int i = 0; i <= p.length(); i++) {
+            String f = p.substring(0, i);
+            String s = p.substring(i, p.length());
+            permutations(f + ch + s, up.substring(1));
+        }
+    }
+}
