e6b1524455f631a4c57d03044d2fb6cf12b336c8
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-09-25 19:53:40 +0530

    added the subSeq problem for all purmutations of string abc

--- _32nd_recursion_subset_subsequence_string_questions/src/com/adnan/SubSeq.java
+++ _32nd_recursion_subset_subsequence_string_questions/src/com/adnan/SubSeq.java
@@ -2,6 +2,16 @@ package com.adnan;
 
 public class SubSeq {
     public static void main(String[] args) {
-
+    String s = "abc";
+    subSeq("",s);
+    }
+    static void subSeq(String p ,String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        subSeq(p+ch, up.substring(1));
+        subSeq(p, up.substring(1));
     }
 }
