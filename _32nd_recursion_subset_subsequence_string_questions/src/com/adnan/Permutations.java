9b79d47104057f5499ddda695a8095736cc46ef6
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-09-26 00:39:15 +0530

    added the permutationslist methos to return list of all possible permutations

--- _32nd_recursion_subset_subsequence_string_questions/src/com/adnan/Permutations.java
+++ _32nd_recursion_subset_subsequence_string_questions/src/com/adnan/Permutations.java
@@ -1,5 +1,7 @@
 package com.adnan;
 
+import java.util.ArrayList;
+
 public class Permutations {
     public static void main(String[] args) {
 
@@ -15,5 +17,24 @@ public class Permutations {
             String s = p.substring(i, p.length());
             permutations(f + ch + s, up.substring(1));
         }
+    }
+
+    static ArrayList<String> permutationsList(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+
+        // local to this call
+        ArrayList<String> ans = new ArrayList<>();
+
+        for (int i = 0; i <= p.length(); i++) {
+            String f = p.substring(0, i);
+            String s = p.substring(i, p.length());
+            ans.addAll(permutationsList(f + ch + s, up.substring(1)));
+        }
+        return ans;
     }
 }
