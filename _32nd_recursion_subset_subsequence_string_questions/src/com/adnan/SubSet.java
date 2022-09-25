d1c0bb18fc3430cef9bc1a380771287ba47f1b19
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-09-25 20:44:30 +0530

    added the subSetDuplicate method which eliminates the duplicates among the subsets

--- _32nd_recursion_subset_subsequence_string_questions/src/com/adnan/SubSet.java
+++ _32nd_recursion_subset_subsequence_string_questions/src/com/adnan/SubSet.java
@@ -1,6 +1,7 @@
 package com.adnan;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SubSet {
@@ -33,5 +34,28 @@ public class SubSet {
         // O(N raised to 2 * N)
     }
 
+    // for eliminating duplicate elements
+    static List<List<Integer>> subsetDuplicate(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < arr.length; i++) {
+            start = 0;
+            // if current and previous element is same, s = e + 1
+            if (i > 0 && arr[i] == arr[i-1]) {
+                start = end + 1;
+            }
+            end = outer.size() - 1;
+            int n = outer.size();
+            for (int j = start; j < n; j++) {
+                List<Integer> internal = new ArrayList<>(outer.get(j));
+                internal.add(arr[i]);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
 
 }
