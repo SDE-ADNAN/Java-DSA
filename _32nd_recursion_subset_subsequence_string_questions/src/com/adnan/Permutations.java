c999ce004a36acbdeb0d27a4465dd1f34cd08e1c
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-09-26 00:40:29 +0530

    added the permutationscount method to return no of permutations possible for a given str

--- _32nd_recursion_subset_subsequence_string_questions/src/com/adnan/Permutations.java
+++ _32nd_recursion_subset_subsequence_string_questions/src/com/adnan/Permutations.java
@@ -37,4 +37,17 @@ public class Permutations {
         }
         return ans;
     }
+    static int permutationsCount(String p, String up) {
+        if (up.isEmpty()) {
+            return 1;
+        }
+        int count = 0;
+        char ch = up.charAt(0);
+        for (int i = 0; i <= p.length(); i++) {
+            String f = p.substring(0, i);
+            String s = p.substring(i, p.length());
+            count = count + permutationsCount(f + ch + s, up.substring(1));
+        }
+        return count;
+    }
 }
