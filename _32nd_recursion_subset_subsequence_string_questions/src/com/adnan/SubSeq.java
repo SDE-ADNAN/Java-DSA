cd7ff5eec3b02bde3b4ed0ca4885023114d7c78a
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-09-25 20:01:46 +0530

    added the subsequences problem for returning a list of all subsequences

--- _32nd_recursion_subset_subsequence_string_questions/src/com/adnan/SubSeq.java
+++ _32nd_recursion_subset_subsequence_string_questions/src/com/adnan/SubSeq.java
@@ -1,9 +1,12 @@
 package com.adnan;
 
+import java.util.ArrayList;
+
 public class SubSeq {
     public static void main(String[] args) {
     String s = "abc";
-    subSeq("",s);
+     ArrayList<String> ans = subSeqList("",s);
+        System.out.println(ans);
     }
     static void subSeq(String p ,String up){
         if(up.isEmpty()){
@@ -13,5 +16,19 @@ public class SubSeq {
         char ch = up.charAt(0);
         subSeq(p+ch, up.substring(1));
         subSeq(p, up.substring(1));
+    }
+
+    // For Returning Array of SubSequences
+    static ArrayList<String>  subSeqList(String p , String up){
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+        ArrayList<String> left = subSeqList(p+ch, up.substring(1));
+        ArrayList<String> right = subSeqList(p, up.substring(1));
+        left.addAll(right);
+        return left;
     }
 }
