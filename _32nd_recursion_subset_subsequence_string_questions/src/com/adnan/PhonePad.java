1e5e629e62dc9d84d0f388b1c447f0ad927a9a59
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-09-26 23:58:55 +0530

    added the phone pad for returning count of all combinations

--- _32nd_recursion_subset_subsequence_string_questions/src/com/adnan/PhonePad.java
+++ _32nd_recursion_subset_subsequence_string_questions/src/com/adnan/PhonePad.java
@@ -37,4 +37,16 @@ public class PhonePad {
         return list;
     }
 
+    static int padCount(String p, String up) {
+        if (up.isEmpty()) {
+            return 1;
+        }
+        int count = 0;
+        int digit = up.charAt(0) - '0'; // this will convert '2' into 2
+        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+            char ch = (char) ('a' + i);
+            count = count + padCount(p + ch, up.substring(1));
+        }
+        return count;
+    }
 }
