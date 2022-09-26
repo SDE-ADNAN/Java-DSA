9c579fa570780f08a3ca86bdb8210c99a6441cc3
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-09-26 23:57:38 +0530

    added the phonepad 17th leetcodeproblem

---
+++ _32nd_recursion_subset_subsequence_string_questions/src/com/adnan/PhonePad.java
@@ -0,0 +1,24 @@
+package com.adnan;
+import java.util.ArrayList;
+
+public class PhonePad {
+    public static void main(String[] args) {
+        System.out.println(padRet("", "12").size());
+        System.out.println(padCount("", "12"));
+    }
+
+    static void pad(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+        int digit = up.charAt(0) - '0'; // this will convert '2' into 2
+        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+            char ch = (char) ('a' + i);
+            pad(p + ch, up.substring(1));
+        }
+
+    }
+
+    
+}
