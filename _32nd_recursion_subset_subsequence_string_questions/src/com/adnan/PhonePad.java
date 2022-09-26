6268b346d4a1c3258b75ce4e79da42b829bade99
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-09-26 23:58:33 +0530

    added the phone pad for returning a list

--- _32nd_recursion_subset_subsequence_string_questions/src/com/adnan/PhonePad.java
+++ _32nd_recursion_subset_subsequence_string_questions/src/com/adnan/PhonePad.java
@@ -20,5 +20,21 @@ public class PhonePad {
 
     }
 
-    
+    static ArrayList<String> padRet(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        int digit = up.charAt(0) - '0'; // this will convert '2' into 2
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+            char ch = (char) ('a' + i);
+            list.addAll(padRet(p + ch, up.substring(1)));
+        }
+        return list;
+    }
+
 }
