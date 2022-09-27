93a970f95de3052456a974d6433ef252b1063db0
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-09-27 22:19:27 +0530

    added the diceRet which returns all the possible combinations into a array

--- _32nd_recursion_subset_subsequence_string_questions/src/com/adnan/Dice.java
+++ _32nd_recursion_subset_subsequence_string_questions/src/com/adnan/Dice.java
@@ -1,5 +1,7 @@
 package com.adnan;
 
+import java.util.ArrayList;
+
 public class Dice {
     public static void main(String[] args) {
         dice("", 4);
@@ -13,6 +15,19 @@ public class Dice {
         for (int i = 1; i <= 6 && i <= target; i++) {
             dice(p + i, target - i);
         }
+    }
+
+    static ArrayList<String> diceRet(String p, int target) {
+        if (target == 0) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 1; i <= 6 && i <= target; i++) {
+            list.addAll(diceRet(p + i, target - i));
+        }
+        return list;
     }
 
 }
