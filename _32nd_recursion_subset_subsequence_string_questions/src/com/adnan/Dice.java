d46ca1f3188d3f7f2bb0de4cd3f5d4b1422b4416
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-09-27 22:18:04 +0530

    added the dice method to give all combinations required to get a number

---
+++ _32nd_recursion_subset_subsequence_string_questions/src/com/adnan/Dice.java
@@ -0,0 +1,18 @@
+package com.adnan;
+
+public class Dice {
+    public static void main(String[] args) {
+        dice("", 4);
+    }
+    static void dice(String p, int target) {
+        if (target == 0) {
+            System.out.println(p);
+            return;
+        }
+
+        for (int i = 1; i <= 6 && i <= target; i++) {
+            dice(p + i, target - i);
+        }
+    }
+
+}
