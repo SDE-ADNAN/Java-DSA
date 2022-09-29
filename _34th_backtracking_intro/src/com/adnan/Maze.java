6aaf65668ff6307ef8fe9030be778ae66b59444b
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-09-29 23:59:12 +0530

    added the pathdiagonal

--- _34th_backtracking_intro/src/com/adnan/Maze.java
+++ _34th_backtracking_intro/src/com/adnan/Maze.java
@@ -58,5 +58,28 @@ public class Maze {
 
         return list;
     }
+    static ArrayList<String> pathRetDiagonal(String p, int r, int c) {
+        if (r == 1 && c == 1) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+
+        if (r > 1 && c > 1) {
+            list.addAll(pathRetDiagonal(p + 'D', r-1, c-1));
+        }
+
+        if (r > 1) {
+            list.addAll(pathRetDiagonal(p + 'V', r-1, c));
+        }
+
+        if (c > 1) {
+            list.addAll(pathRetDiagonal(p + 'H', r, c-1));
+        }
+
+        return list;
+    }
 
 }
