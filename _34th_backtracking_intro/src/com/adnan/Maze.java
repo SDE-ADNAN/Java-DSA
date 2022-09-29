130948f20a319108281c55847ac45f168f8e4d69
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-09-29 23:58:23 +0530

    added the pathRet func to return all possible paths in a arraylst

--- _34th_backtracking_intro/src/com/adnan/Maze.java
+++ _34th_backtracking_intro/src/com/adnan/Maze.java
@@ -1,5 +1,7 @@
 package com.adnan;
 
+import java.util.ArrayList;
+
 public class Maze {
     public static void main(String[] args) {
         //        System.out.println(count(3, 3));
@@ -37,4 +39,24 @@ public class Maze {
             path(p + 'R', r, c-1);
         }
     }
+    static ArrayList<String> pathRet(String p, int r, int c) {
+        if (r == 1 && c == 1) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+
+        if (r > 1) {
+            list.addAll(pathRet(p + 'D', r-1, c));
+        }
+
+        if (c > 1) {
+            list.addAll(pathRet(p + 'R', r, c-1));
+        }
+
+        return list;
+    }
+
 }
