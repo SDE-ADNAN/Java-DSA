0c0e970de56f7531d6d712552f32a2223922558f
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-09-30 00:04:31 +0530

    added the wrong one allpath func

---
+++ _34th_backtracking_intro/src/com/adnan/AllPaths.java
@@ -0,0 +1,54 @@
+package com.adnan;
+
+import java.util.Arrays;
+
+public class AllPaths {
+    public static void main(String[] args) {
+        boolean[][] board = {
+                {true, true, true},
+                {true, true, true},
+                {true, true, true}
+        };
+        int[][] path = new int[board.length][board[0].length];
+//        allPathPrint("", board, 0, 0, path, 1);
+    }
+
+    // wrong one with repeated recursive calls and stackoverflow error
+    static void allPath(String p, boolean[][] maze, int r, int c) {
+        if (r == maze.length - 1 && c == maze[0].length - 1) {
+            System.out.println(p);
+            return;
+        }
+
+        if (!maze[r][c]) {
+            return;
+        }
+
+        // i am considering this block in my path
+        maze[r][c] = false;
+
+        if (r < maze.length - 1) {
+            allPath(p + 'D', maze, r+1, c);
+        }
+
+        if (c < maze[0].length - 1) {
+            allPath(p + 'R', maze, r, c+1);
+        }
+
+        if (r > 0) {
+            allPath(p + 'U', maze, r-1, c);
+        }
+
+        if (c > 0) {
+            allPath(p + 'L', maze, r, c-1);
+        }
+
+        // this line is where the function will be over
+        // so before the function gets removed, also remove the changes that were made by that function
+        maze[r][c] = true;
+    }
+
+
+
+
+}
