a6a1c41856705326833924a6ec35ca3f145cfe85
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-09-29 23:57:10 +0530

    added the path function to print all possible paths to reach target

--- _34th_backtracking_intro/src/com/adnan/Maze.java
+++ _34th_backtracking_intro/src/com/adnan/Maze.java
@@ -25,4 +25,16 @@ public class Maze {
         int right = count(r, c-1);
         return left + right;
     }
+    static void path(String p, int r, int c) {
+        if (r == 1 && c == 1) {
+            System.out.println(p);
+            return;
+        }
+        if (r > 1) {
+            path(p + 'D', r-1, c);
+        }
+        if (c > 1) {
+            path(p + 'R', r, c-1);
+        }
+    }
 }
