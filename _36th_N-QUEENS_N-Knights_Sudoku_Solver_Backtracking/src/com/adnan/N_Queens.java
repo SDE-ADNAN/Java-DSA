351740ecf8cc529b578337ed1cf1a1dd18fa9541
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-10-02 14:30:24 +0530

    added the display method to print the resultant matrix with possible solution

--- _36th_N-QUEENS_N-Knights_Sudoku_Solver_Backtracking/src/com/adnan/N_Queens.java
+++ _36th_N-QUEENS_N-Knights_Sudoku_Solver_Backtracking/src/com/adnan/N_Queens.java
@@ -4,4 +4,16 @@ public class N_Queens {
     public static void main(String[] args) {
 
     }
+    private static void display(boolean[][] board) {
+        for(boolean[] row : board) {
+            for(boolean element : row) {
+                if (element) {
+                    System.out.print("Q ");
+                } else {
+                    System.out.print("X ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
