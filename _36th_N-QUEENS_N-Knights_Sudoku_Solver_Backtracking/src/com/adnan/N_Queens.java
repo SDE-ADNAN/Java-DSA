5e2b0faac26af19cde0f4ed44d0baa89023c322f
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-10-02 14:33:05 +0530

    added the queens method fr running the whole logic

--- _36th_N-QUEENS_N-Knights_Sudoku_Solver_Backtracking/src/com/adnan/N_Queens.java
+++ _36th_N-QUEENS_N-Knights_Sudoku_Solver_Backtracking/src/com/adnan/N_Queens.java
@@ -4,6 +4,27 @@ public class N_Queens {
     public static void main(String[] args) {
 
     }
+    static int queens(boolean[][] board, int row) {
+        if (row == board.length) {
+            display(board);
+            System.out.println();
+            return 1;
+        }
+
+        int count = 0;
+
+        // placing the queen and checking for every row and col
+        for (int col = 0; col < board.length; col++) {
+            // place the queen if it is safe
+            if(isSafe(board, row, col)) {
+                board[row][col] = true;
+                count += queens(board, row + 1);
+                board[row][col] = false;
+            }
+        }
+
+        return count;
+    }
     private static boolean isSafe(boolean[][] board, int row, int col) {
         // check vertical row
         for (int i = 0; i < row; i++) {
