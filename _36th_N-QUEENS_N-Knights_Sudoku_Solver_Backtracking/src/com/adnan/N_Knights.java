ce9be4a93bdf7233e33fe48d86a73753fb2946f4
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-10-02 16:08:25 +0530

    added the isValid Check to avoid repeatitions

--- _36th_N-QUEENS_N-Knights_Sudoku_Solver_Backtracking/src/com/adnan/N_Knights.java
+++ _36th_N-QUEENS_N-Knights_Sudoku_Solver_Backtracking/src/com/adnan/N_Knights.java
@@ -4,6 +4,13 @@ public class N_Knights {
     public static void main(String[] args) {
 
     }
+    // do not repeat yourself, hence created this function
+    static boolean isValid(boolean[][] board, int row, int col) {
+        if (row >= 0 && row < board.length && col >= 0 && col < board.length) {
+            return true;
+        }
+        return false;
+    }
     private static void display(boolean[][] board) {
         for(boolean[] row : board) {
             for(boolean element : row) {
