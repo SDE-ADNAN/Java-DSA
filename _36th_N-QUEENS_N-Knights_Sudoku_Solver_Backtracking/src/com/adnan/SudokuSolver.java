cba5bfdea1dc2abe6b30081649d26cdf3aff9227
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-10-02 19:21:03 +0530

    added the solve method to compare the column , row and also the individual box for num ranging from 1 to 9

--- _36th_N-QUEENS_N-Knights_Sudoku_Solver_Backtracking/src/com/adnan/SudokuSolver.java
+++ _36th_N-QUEENS_N-Knights_Sudoku_Solver_Backtracking/src/com/adnan/SudokuSolver.java
@@ -5,6 +5,50 @@ public class SudokuSolver {
 
     }
 
+    static boolean solve(int[][] board) {
+        int n = board.length;
+        int row = -1;
+        int col = -1;
+
+        boolean emptyLeft = true;
+
+        // this is how we are replacing the r,c from arguments
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 0) {
+                    row = i;
+                    col = j;
+                    emptyLeft = false;
+                    break;
+                }
+            }
+            // if you found some empty element in row, then break
+            if (emptyLeft == false) {
+                break;
+            }
+        }
+
+        if (emptyLeft == true) {
+            return true;
+            // soduko is solved
+        }
+
+        // backtrack
+        for (int number = 1; number <= 9; number++) {
+            if (isSafe(board, row, col, number)) {
+                board[row][col] = number;
+                if (solve(board)) {
+                    // found the answer
+                    return true;
+                } else {
+                    // backtrack
+                    board[row][col] = 0;
+                }
+            }
+        }
+        return false;
+    }
+
     private static void display(int[][] board) {
         for(int[] row : board) {
             for(int num : row) {
