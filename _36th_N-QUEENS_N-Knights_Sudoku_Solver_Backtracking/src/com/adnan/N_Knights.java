b256ef31611ee5bb21c4a70abc45887d213441fc
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-10-02 16:07:24 +0530

    added the display method to print the resultant matrix with possible solution

--- _36th_N-QUEENS_N-Knights_Sudoku_Solver_Backtracking/src/com/adnan/N_Knights.java
+++ _36th_N-QUEENS_N-Knights_Sudoku_Solver_Backtracking/src/com/adnan/N_Knights.java
@@ -4,5 +4,16 @@ public class N_Knights {
     public static void main(String[] args) {
 
     }
-
+    private static void display(boolean[][] board) {
+        for(boolean[] row : board) {
+            for(boolean element : row) {
+                if (element) {
+                    System.out.print("K ");
+                } else {
+                    System.out.print("X ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
