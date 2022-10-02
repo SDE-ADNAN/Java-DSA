1e3132ad97ad65e73fafaadf29c10fa500c2e115
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-10-02 16:09:21 +0530

    added the isSafe method

--- _36th_N-QUEENS_N-Knights_Sudoku_Solver_Backtracking/src/com/adnan/N_Knights.java
+++ _36th_N-QUEENS_N-Knights_Sudoku_Solver_Backtracking/src/com/adnan/N_Knights.java
@@ -4,6 +4,33 @@ public class N_Knights {
     public static void main(String[] args) {
 
     }
+    private static boolean isSafe(boolean[][] board, int row, int col) {
+        if (isValid(board, row - 2, col - 1)) {
+            if (board[row - 2][col - 1]) {
+                return false;
+            }
+        }
+
+        if (isValid(board, row - 1, col - 2)) {
+            if (board[row - 1][col - 2]) {
+                return false;
+            }
+        }
+
+        if (isValid(board, row - 2, col + 1)) {
+            if (board[row - 2][col + 1]) {
+                return false;
+            }
+        }
+
+        if (isValid(board, row - 1, col + 2)) {
+            if (board[row - 1][col + 2]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
     // do not repeat yourself, hence created this function
     static boolean isValid(boolean[][] board, int row, int col) {
         if (row >= 0 && row < board.length && col >= 0 && col < board.length) {
