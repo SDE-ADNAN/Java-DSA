bcd153864cc7250baeab217fa72918e5a7fcdf90
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-10-02 19:18:51 +0530

    added the isSafe method in the sudoku solver problem

--- _36th_N-QUEENS_N-Knights_Sudoku_Solver_Backtracking/src/com/adnan/SudokuSolver.java
+++ _36th_N-QUEENS_N-Knights_Sudoku_Solver_Backtracking/src/com/adnan/SudokuSolver.java
@@ -4,4 +4,35 @@ public class SudokuSolver {
     public static void main(String[] args) {
 
     }
+
+    static boolean isSafe(int[][] board, int row, int col, int num) {
+        // check the row
+        for (int i = 0; i < board.length; i++) {
+            // check if the number is in the row
+            if (board[row][i] == num) {
+                return false;
+            }
+        }
+
+        // check the col
+        for (int[] nums : board) {
+            // check if the number is in the col
+            if (nums[col] == num) {
+                return false;
+            }
+        }
+
+        int sqrt = (int)(Math.sqrt(board.length));
+        int rowStart = row - row % sqrt;
+        int colStart = col - col % sqrt;
+
+        for (int r = rowStart; r < rowStart + sqrt; r++) {
+            for (int c = colStart; c < colStart + sqrt; c++) {
+                if (board[r][c] == num) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
