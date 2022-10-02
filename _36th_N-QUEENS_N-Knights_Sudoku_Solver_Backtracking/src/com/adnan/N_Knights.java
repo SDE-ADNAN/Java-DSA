311013a654c394fb6b2e0423b75bfca5ff799985
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-10-02 16:10:13 +0530

    added the knights method to sumup the logic

--- _36th_N-QUEENS_N-Knights_Sudoku_Solver_Backtracking/src/com/adnan/N_Knights.java
+++ _36th_N-QUEENS_N-Knights_Sudoku_Solver_Backtracking/src/com/adnan/N_Knights.java
@@ -4,6 +4,30 @@ public class N_Knights {
     public static void main(String[] args) {
 
     }
+    static void knight(boolean[][] board, int row, int col, int knights) {
+        if (knights == 0) {
+            display(board);
+            System.out.println();
+            return;
+        }
+
+        if (row == board.length - 1 && col == board.length) {
+            return;
+        }
+
+        if (col == board.length) {
+            knight(board, row + 1, 0, knights);
+            return;
+        }
+
+        if (isSafe(board, row, col)) {
+            board[row][col] = true;
+            knight(board, row, col + 1, knights - 1);
+            board[row][col] = false;
+        }
+
+        knight(board, row, col + 1, knights);
+    }
     private static boolean isSafe(boolean[][] board, int row, int col) {
         if (isValid(board, row - 2, col - 1)) {
             if (board[row - 2][col - 1]) {
