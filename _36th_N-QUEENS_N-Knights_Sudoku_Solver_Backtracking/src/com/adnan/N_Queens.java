b3144b0b5507d302e383bf010ec956cc0a380c4f
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-10-02 14:31:44 +0530

    added the isSafe method to find if the cell is safe to place the next queen

--- _36th_N-QUEENS_N-Knights_Sudoku_Solver_Backtracking/src/com/adnan/N_Queens.java
+++ _36th_N-QUEENS_N-Knights_Sudoku_Solver_Backtracking/src/com/adnan/N_Queens.java
@@ -4,6 +4,32 @@ public class N_Queens {
     public static void main(String[] args) {
 
     }
+    private static boolean isSafe(boolean[][] board, int row, int col) {
+        // check vertical row
+        for (int i = 0; i < row; i++) {
+            if (board[i][col]) {
+                return false;
+            }
+        }
+
+        // diagonal left
+        int maxLeft = Math.min(row, col);
+        for (int i = 1; i <= maxLeft; i++) {
+            if(board[row-i][col-i]) {
+                return false;
+            }
+        }
+
+        // diagonal right
+        int maxRight = Math.min(row, board.length - col - 1);
+        for (int i = 1; i <= maxRight; i++) {
+            if(board[row-i][col+i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
     private static void display(boolean[][] board) {
         for(boolean[] row : board) {
             for(boolean element : row) {
